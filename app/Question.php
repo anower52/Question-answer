f95dd6b2b4c57371edd9eff9755d5c53bcf52968
anower00 <anower.hasan@gmail.com>
2020-01-22 23:43:51 +0600

    add author info and question creation data in question item

--- app/Question.php
+++ app/Question.php
@@ -11,11 +11,20 @@ class Question extends Model
 
     public function user()
     {
-        $this->belongsTo(User::class);
+       return $this->belongsTo(User::class);
     }
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
+    }
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
